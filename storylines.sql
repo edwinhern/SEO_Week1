c336b5a0d33f67c5cccc06f9b76af5660317329e
Edwin Hernandez <edwinhern.16@gmail.com>
2021-07-02 16:46:50 +0000

    Updated code

--- storylines.sql
+++ storylines.sql
@@ -16,28 +16,26 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `stories`
+-- Table structure for table `new_stories`
 --
 
-DROP TABLE IF EXISTS `stories`;
+DROP TABLE IF EXISTS `new_stories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `stories` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) DEFAULT NULL,
-  `author` varchar(255) DEFAULT NULL,
-  `date_added` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `new_stories` (
+  `Title` text DEFAULT NULL,
+  `Author` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `stories`
+-- Dumping data for table `new_stories`
 --
 
-LOCK TABLES `stories` WRITE;
-/*!40000 ALTER TABLE `stories` DISABLE KEYS */;
-/*!40000 ALTER TABLE `stories` ENABLE KEYS */;
+LOCK TABLES `new_stories` WRITE;
+/*!40000 ALTER TABLE `new_stories` DISABLE KEYS */;
+INSERT INTO `new_stories` VALUES ('Journeyactions – Travelling Made Easier','dutcroyal');
+/*!40000 ALTER TABLE `new_stories` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -49,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-01 19:19:39
+-- Dump completed on 2021-07-02 16:45:17
--- test.py
+++ test.py
@@ -1,55 +1,73 @@
-import sqlalchemy, json, requests
-from sqlalchemy import create_engine
+import sqlalchemy, json, requests, os
+from sqlalchemy import create_engine 
 import pandas as pd
 
-# Retreives id numbers of newstories and converts into json 
-# Testing 1: Make sure API is working, if 200 shows up-> works, else-> broken
-# Testing 2: Make sure the url isnt empty, if empty, return error statment
-# Testing 3: Make sure the return value is a list (Output should be list of values)
-def convert_to_json(url):
-    r = requests.get(url).json()
-    return r
-
-# Grabs the first index in the json and creates the url link to the new story
-# Testing 1: Check if 'id' is a number
-# Testing 2: Check if url works, if null-> doesn't work
-# Testing 3: 
-def specifc_new_story_url(response):
-    id = response[0]
-    story_url = "https://hacker-news.firebaseio.com/v0/item/"+str(id)+".json?print=pretty"
-    return story_url
+def pullDataFromAPIintoPandasDF():
+  col_names = ['Title', 'Author']
+  dataframe  = pd.DataFrame(columns = col_names)
   
-# Converts the new story information to a json
-# Testing 1: Make sure the info (Title, author) you want is in the json file 
-# Testing 2: Make sure return value so dict., else create error statment
-# Testing 3: 
-def new_url_to_json(url):
-  response = requests.get(url).json()
-  return response
+  response = requests.get("https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty")
+  mostRecent = response.json()
+  
+  dataframe = getDataFromAPI(dataframe, mostRecent)
+  
+  print(dataframe)
+  return dataframe
+  
+def getDataFromAPI(dataframe, mostRecent):
+    id = mostRecent[0]
+    story_url = "https://hacker-news.firebaseio.com/v0/item/"+str(id)+".json?print=pretty"
+    json_file = requests.get(story_url).json()
+    dataframe.loc[len(dataframe.index)] = [json_file["title"], json_file["by"]]
+    return dataframe
 
-# Creates a table and returns DataFrame
-# Testing 1: Check if the values or null, if so return NONE
-# Testing 2: 
-def create_table(json_file):
-    col_names = ['Title', 'Author']
-    df = pd.DataFrame(columns = col_names)
-    df.loc[len(df.index)] = [json_file["title"], json_file["by"]]
-    return df
+def loadNewData(dataframe):
+    dataframe.sort_values(by='Story_ID', inplace=True, ascending=False)
+    neweststorysaved = dataframe.iloc[0,0]
+    
+    response = requests.get("https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty")
+    mostRecent = response.json()
+    
+    dataframe = getDataFromAPI(dataframe, mostRecent)
+    dataframe.sort_values(by='Story_ID', inplace=True, ascending=False)
+    return dataframe
+  
+# Generalized method for migrating data     
+ 
+def saveSQLtoFile(filename, database_name):
+    os.system('mysqldump -u root -pcodio '+database_name+' > '+ filename)
 
-# Exports dataframe to mysql
-# Testing 1: Check if database is created
-# Testing 2: 
-def export_to_mysql(df):
-    engine = create_engine('mysql://root:codio@localhost/test')
-    df.to_sql('test', con=engine, if_exists='replace', index=False)
+def loadSQLfromFile(filename, database_name):
+    #create database if it does not exist
+    os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS '+database_name+';"')
+    os.system("mysql -u root -pcodio "+database_name+" < " + filename)
+  
+def createEngine(database_name):
+    return create_engine('mysql://root:codio@localhost/'+database_name+'?charset=utf8', encoding='utf-8')
+    
+def saveDatasetToFile(database_name, table_name, filename, dataframe):
+    dataframe.to_sql(table_name, con=createEngine(database_name), if_exists='replace', index=False)
+    saveSQLtoFile(filename, database_name)
 
+def loadDataset(database_name, table_name, filename, update=False):
+    loadSQLfromFile(filename, database_name)
+    df = pd.read_sql_table(table_name, con=createEngine(database_name))
+    if update:
+        return loadNewData(df)
+    else:
+        return df
+    
 # Main function
 if __name__ == "__main__":
-    # API link to number of newstories
-    url = "https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty"
+    database_name = "articles"
+    table_name = "new_stories"
+    filename = "storylines.sql"
+  
+    df = pullDataFromAPIintoPandasDF()
+    saveDatasetToFile(database_name, table_name, filename, df)
     
-    json_file = convert_to_json(url)
-    new_story_url = specifc_new_story_url(json_file)
-    new_story_json_file = new_url_to_json(new_story_url)
-    data_frame = create_table(new_story_json_file)
-    export_to_mysql(data_frame)
+    df = loadDataset(database_name, table_name, filename)
+    saveDatasetToFile(database_name, table_name, filename, df)
+    
+    
+    
